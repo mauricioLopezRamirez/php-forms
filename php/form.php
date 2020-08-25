3f7b1ed1af88c5d44406551ce5d62d8516b226a9
Mauricio <mauricio.lopez@umanizales.edu.co>
2020-08-25 14:49:31 -0500

    se agrego campo y validaciones

--- php/form.php
+++ php/form.php
@@ -1,16 +1,22 @@
 <?php
 $first_name = $_POST['first_name'];
-$last_name = $_POST['last_name'];
-$password = $_POST['password'];
-$email = $_POST['email'];
-$color = $_POST['color'];
-$terms = $_POST['terms'];
+$last_name  = $_POST['last_name'];
+$password   = $_POST['password'];
+$email      = $_POST['email'];
+$phone      = $_POST['phone'];
+$color      = $_POST['color'];
+$terms      = $_POST['terms'];
 
 //show information
-echo 'first_name: '.$first_name;
-echo '<br>last_name: '.$last_name;
-echo '<br>password: '.$password;
-echo '<br>email: '.$email;
-echo '<br>color: '.$color;
-echo '<br>terms: '.$terms;
+if ($first_name == "" or $last_name == "" or $password == "" or $email == "" or $color == "" or $terms == "") {
+    echo "Complete todos los campos";
+} else {
+    echo 'first_name: ' . $first_name;
+    echo '<br>last_name: ' . $last_name;
+    echo '<br>password: ' . $password;
+    echo '<br>email: ' . $email;
+    echo '<br>phone: ' . $phone;
+    echo '<br>color: ' . $color;
+    echo '<br>terms: ' . $terms;
+}
 ?>
